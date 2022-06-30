273b60cf293f5a08464a314d14d02f76a37f54f0
Aubree Alexander <aubree.alexander@gmail.com>
2022-06-30 18:28:16 -0500

    add web manifest

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
 const path = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 
 
 const config = {
@@ -44,9 +45,23 @@ const config = {
             $: "jquery",
             jQuery: "jquery"
           }),
-          new BundleAnalyzerPlugin({
+        new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-          })
+        }),
+        new WebpackPwaManifest({
+            name: 'Food Event',
+            short_name: 'Foodies',
+            description: 'An app that allows you to view upcoming food events.',
+            start_url: '../index.html',
+            background_color: '#ffffff',
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve('assets/img/icons/icon-512x512.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons')
+            }]
+        })
     ],
     mode: 'development'
 }
